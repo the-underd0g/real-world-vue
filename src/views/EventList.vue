51a3877825daa6da3694ddb2920052d7e745e33f
underd0g <underd0g.3003@gmail.com>
2019-04-12 12:50:43 -0300

    adding the api call to the mockk server

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,19 +1,36 @@
 <template>
     <div>
         <h1>Events Listing</h1>
-        <EventCard/>
+        <EventCard v-for="event in events" :key="event.id" :event="event"/>
     </div>
 </template>
 
 <script>
     import EventCard from '@/components/EventCard.vue';
+    import axios from 'axios';
 
     export default {
         name: "EventList",
         components: {
             EventCard
+        },
+        data() {
+            return {
+                events: []
+            }
+        },
+        created() {
+            axios.get('http:://localhost:3000/events')
+                .then(response => {
+                    this.events = response;
+                })
+                .catch(error => {
+                    console.log(`${error.response}` );
+                })
         }
     }
+
+
 </script>
 
 <style scoped>
