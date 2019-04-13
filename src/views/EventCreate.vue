b1629e721c84f4f1ce7cb73d0b1a577fb19a2757
underd0g <underd0g.3003@gmail.com>
2019-04-13 14:32:58 -0300

    implementation code for the event creation

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Create Event, {{ userName }}</h1>
-  <form>
+  <form @submit.prevent="createEvent">
     <label>Select a category</label>
     <select v-model="event.category">
       <option v-for="cat in categories" :key="cat">{{ cat }}</option>
@@ -53,12 +53,53 @@ export default {
   name: 'EventCreate',
   components: {
     Datepicker
+  },
+  data() {
+    const times = []
+    for (let i = 1; i <= 24; i++) {
+      times.push(i + ':00')
+    }
+    return {
+      times,
+      categories: this.$store.state.categories,
+      event: this.createFreshEventObject()
+    }
+  },
+  methods: {
+    createEvent() {
+      this.$store.dispatch('createEvent', this.event).then(() => {
+        this.$router.push({
+            name: 'event-show',
+            params: { id: this.event.id }
+        })
+        this.event = this.createFreshEventObject()
+      }).
+          catch(() => {
+              console.log("there was a problem creating your event")
+      })
+    },
+    createFreshEventObject() {
+      const user = this.$store.state.user
+      const id = Math.floor(Math.random() * 10000000)
+      return {
+        id: id,
+        user: user,
+        category: '',
+        organizer: '',
+        title: '',
+        description: '',
+        location: '',
+        date: '',
+        time: '',
+        attendees: []
+      }
+    }
   }
 }
 </script>
 
 <style scoped>
 .field {
-    margin-bottom: 24px;
+  margin-bottom: 24px;
 }
 </style>
