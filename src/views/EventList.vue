2a19e65696d8cc9f83ced052dae3657af9738712
underd0g <underd0g.3003@gmail.com>
2019-04-14 10:18:07 -0300

    adding first implementation of user module

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Events Listing</h1>
+    <h1>Events for {{ user.user.name }}</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <template v-if="page != 1">
       <router-link :to="{ name: 'event-list', query: { page: page -1 } }" rel="prev">Prev Page</router-link> |
@@ -25,7 +25,7 @@ export default {
     page() {
       return parseInt(this.$route.query.page) || 1
     },
-    ...mapState(['events'])
+    ...mapState(['events', 'eventsTotal', 'user'])
   }
 }
 </script>
